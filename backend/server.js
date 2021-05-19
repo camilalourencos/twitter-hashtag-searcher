cf1384c110eb4b488ff6552dd307ee1171a58a9e
Camila Lourenco de Souza <camila.lourenco@ibm.com>
2021-05-19 19:58:09 -0300

    commit Update server.js to include comments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const { TwitterApi } = require('twitter-api-v2');
 
-//login to the API via 
+//login to TwitterAPI
 const consumerClient = new TwitterApi({ 
     appKey: 'DJ8ZkyMlHtXr7B1sWQlbPipiN', 
     appSecret: 'ZHGNaXyga1cONRYaSQIX7obzXoJdkdsnKk7i9ZNMJmAHiYxnMa', 
@@ -9,10 +9,10 @@ const consumerClient = new TwitterApi({
     });
 
 
-//search tweets
+//searching for the most recent tweets
 async function getTweets(){
-    const results = await consumerClient.v2.get('tweets/search/recent', {query: 'CPI', max_results: '10'});
-    console.log('olá!', results);
+    const results = await consumerClient.v2.get('tweets/search/recent', {query: '#'+'BBB21', max_results: '10'});
+    console.log(results);
 }
 
 getTweets()
