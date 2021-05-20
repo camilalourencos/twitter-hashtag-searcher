651a3981665678df58df6d69f06a064420edf0ee
Camila Lourenco de Souza <camila.lourenco@ibm.com>
2021-05-19 21:13:51 -0300

    commit adding a box to html for the results

--- frontend/src/app/search/new-search.component.ts
+++ frontend/src/app/search/new-search.component.ts
@@ -1,5 +1,6 @@
 import { style } from "@angular/animations";
 import { Component } from "@angular/core";
+import { FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-new-search',
@@ -8,8 +9,14 @@ import { Component } from "@angular/core";
 })
 export class NewSearchComponent {
 
+  searchValue: string = 'Osório';
+  tweetFound: string = '';
+
+
   hashtagSearch(){
     console.log('ola')
+    console.log('Search for', this.searchValue);
+    console.log('This was found', this.tweetFound);
 
   }
 
