428b0f623bbb9719b96717d803c97e33b8418cb1
Camila Lourenco de Souza <camila.lourenco@ibm.com>
2021-05-21 20:17:04 -0300

    commit removing Twitter personal keys

--- backend/getTweets.js
+++ backend/getTweets.js
@@ -3,10 +3,10 @@ const getUserInput = require('./client/src/app/search/new-search.component.ts');
 
     //login to TwitterAPI
     const consumerClient = new TwitterApi({ 
-    appKey: 'DJ8ZkyMlHtXr7B1sWQlbPipiN', 
-    appSecret: 'ZHGNaXyga1cONRYaSQIX7obzXoJdkdsnKk7i9ZNMJmAHiYxnMa', 
-    accessToken: '3598634896-UcwATP9AGGocpRJF4xsgMMRvFoh8tXF4pxe7ojc', 
-    accessSecret: 'dAPiANCscuMORtjui1wRBnWSSiuDDhFceBrc3Ir4W7GWP', 
+    appKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 
+    appSecret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 
+    accessToken: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 
+    accessSecret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 
     });
 
 
